23cd31e29650d6841493529e390cbeb02406f3f2
barivikranth36 <barivikranth36@gmail.com>
2023-03-30 12:13:35 +0530

    Added API to delete Appointment based on patient Id.[updated]

--- src/main/java/com/shield/eaarogya/Controller/AppointmentController.java
+++ src/main/java/com/shield/eaarogya/Controller/AppointmentController.java
@@ -42,7 +42,7 @@ public class AppointmentController {
     // -------------------------- Delete a Particular Appointment based on patient Id----------------------------
     @DeleteMapping("/deleteAppointmentByPatientId/{patientId}")
     public boolean deleteAppointmentByPatientId(@PathVariable String patientId) {
-        return appointmentService.deleteAppointment(Long.parseLong(patientId));
+        return appointmentService.deleteAppointmentByPatientId(Long.parseLong(patientId));
     }
 
     // -------------------------------- Patients waiting based on appointment Id ---------------------------------------------
